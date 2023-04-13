73fcf3d9796baaea3b3f112e46d9cdf53a3f2659
Pawel Wujczyk <pwujczyk@hotmail.com>
2023-04-13 08:57:08 +0200

    button reference works

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-
-import './App.css';
+import "./App.css";
+import { Button } from "productivitytools.learning.typescript.npmpackage.button";
 
 function App() {
-  return (
-    <div className="App">
-    hello
-    </div>
-  );
+  return <div className="App">hello
+  <Button backgroundColor="red">hello button</Button>
+  </div>;
 }
 
 export default App;
